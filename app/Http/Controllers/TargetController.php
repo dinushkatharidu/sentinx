2998acd15b98769b30973b68403d9dd4e49d204b
dinushkatharidu <asdinushkatharidu@gmail.com>
2026-04-20 21:44:30 +0530

    feat: add routes and target controller logic

--- app/Http/Controllers/TargetController.php
+++ app/Http/Controllers/TargetController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Target;
 use Illuminate\Http\Request;
+use Pest\Arch\ValueObjects\Targets;
 
 class TargetController extends Controller
 {
-    //
+    public function index(){
+        $targets = Target::latest()->get();
+        return view('targets.index', compact('targets'));
+    }
+
+    public function create(){
+        return view('targets.create');
+    }
+
+    public function store(Request $request){
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'username' => 'nullable|string||max:255',
+            'email' => 'nullable|email',
+            'notes' => 'nullable|string',
+        ]);
+
+        Target::create($validated);
+
+        return redirect()->route('targets.index')->with('success', 'Target added successfully!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\TargetController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/targets', [TargetController::class, 'index'])->name('targets.index');
+
+Route::get('targets/create', [TargetController::class, 'create'])->name('targets.create');
+
+Route::post('/targets', [TargetController::class, 'store'])->name('target.store');
+
+
